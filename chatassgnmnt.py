93f233523fa9146b87a601d97da544421db28699
Prabhanjan Bhat <117516541+prabh1202@users.noreply.github.com>
2022-11-09 23:50:51 +0530

    Add files via upload

---
+++ chatassgnmnt.py
@@ -0,0 +1,31 @@
+print("Type '2' to Reschedule an Appointment")
+tkn = int(input("Enter your token Number "))
+if tkn == 17:
+    print("\n You've Booked Morning 7A.M Session \n Verifying Your Registered Mobile Number \n 5 4 3 2 1.....\n The number has been Successfully verified")
+    print("\n You can reschedule your Appointment")
+elif tkn == 18:
+    print("\n You've Booked Morning 8A.M Session \n Verifying Your Registered Mobile Number \n 5 4 3 2 1.....\n The number has been Successfully verified")
+    print("\n You can reschedule your Appointment")
+elif tkn == 19:
+    print("\n You've Booked Morning 9A.M Session \n Verifying Your Registered Mobile Number \n 5 4 3 2 1.....\n The number has been Successfully verified")
+    print("\n You can reschedule your Appointment")
+elif tkn == 20:
+    print("\n You've Booked Afternoon 2P.M Session \n Verifying Your Registered Mobile Number \n 5 4 3 2 1.....\n The number has been Successfully verified")
+    print("\n You can reschedule your Appointment")
+elif tkn == 21:
+    print("\n You've Booked Afternoon 3P.M Session \n Verifying Your Registered Mobile Number \n 5 4 3 2 1.....\n The number has been Successfully verified")
+    print("\n You can reschedule your Appointment")
+elif tkn == 22:
+    print("\n You've Booked Afternoon 3P.M Session \n Verifying Your Registered Mobile Number \n 5 4 3 2 1.....\n The number has been Successfully verified")
+    print("\n You can reschedule your Appointment")
+elif tkn == 23:
+    print("\n You've Booked Evening 6P.M Session \n Verifying Your Registered Mobile Number \n 5 4 3 2 1.....\n The number has been Successfully verified")
+    print("\n You can reschedule your Appointment")
+elif tkn == 24:
+    print("\n You've Booked Eveneing 7P.M Session \n Verifying Your Registered Mobile Number \n 5 4 3 2 1.....\n The number has been Successfully verified")
+    print("\n You can reschedule your Appointment")
+elif tkn == 25:
+    print("\n You've Booked Evening 8P.M Session \n Verifying Your Registered Mobile Number \n 5 4 3 2 1.....\n The number has been Successfully verified")
+    print("\n You can reschedule your Appointment")
+else:
+    print("\n Token number is not available \n Please go through our Website and get an appointment \n THANK YOU!")
