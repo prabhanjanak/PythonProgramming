e3cf6ffa6558660d2fc60c06dbb2aa9231c20caf
Prabhanjan Bhat <117516541+prabh1202@users.noreply.github.com>
2022-11-09 20:11:54 +0530

    Add files via upload

---
+++ jathrechatpy.py
@@ -0,0 +1,36 @@
+print("Hey! I am Chatboy")
+name=input("What's your name")
+print("Hey  ",name,"\n In what do you need help duh!")
+print("Type 'Purchase' for Purchase related quieries")
+print("Type 'Download' for Download related quieries")
+print("Type 'Update' for Update related quieries")
+print("Type 'Uninstall' for Uninstall related quieries")
+print("Type 'Other' for Other related quieries")
+print("Type 'More' for more information")
+print("Type 'Contact' to contact us")
+print("Type 'Log in' to Log in to our community")
+problem= input("What help you need: ")
+if problem == "Purchase":
+    print("If you are not able to do payment using the link Np! You can pay to my UPI Id: chatboy@idfcbank")
+    print("DON'T FORGET TO SEND THE UTR TO Chatboy@gmail.com from your REGISTERED MAIL ID \n ILLA ANDRE YOUTUBE AL HUDUK GURU YAVANO PIRATED VERSION HAKIRTHANE \n DHUD YAK WASTE MADTHYA NAN UPI KODTHINI KALSBUDU")
+elif problem == "Download":
+    print("Please check your Internet Connection \n Try downloading from the provided link www.chatboy.com/product/directdownload \n Please provide your REGISTERED MAIL ID we will share the required download link")
+    mailid=input("REGISTERED MAIL ID(if solved type NA): ")
+    if mailid == "NA":
+        print("We are happy we could help \n DIALOG HODYAK HELUDRU GURU")
+    else:
+        print("You'll recieve the mail in 5 mins \n SWALPA LATE ADRE BEJAR MADKOBEDA \n ILLA INTERNET ON MADIDYA NODO")
+elif problem == "Update":
+    print("This would not work if you use pirated version \n PURHASE THE ORIGINAL VERSION")
+elif problem == "Uninstall":
+    print("If you dont know this why did u install this bloody fellow, we have the video on our YOUTUBE channel \n PLEASE DO SUBSCRIBE LIKE AND SHARE")
+elif problem == "Other":
+    print("Do send the MAIL at 'chatboy@gmail.com \n ADRE REPLY MADTHIVI BEJAR MADKOBEDA SWALPA LATE AGUTHE \n NAN KASHTA NU ARTHA MADKO MACHA")
+elif problem == "More":
+    print("For more info please tap on the top left button,\n AADRU INFORMATION ILLA ANDRE ATAADAKK  DOWWNLOAD MADDYA HAJAMA")
+elif problem == "Contact":
+    print("Contact madbedi nam appa tumbaa strict amele Nange tumbaa NAACHKE AGUTTE Riiiiiii.........")
+elif problem == "Log in":
+    print("Login aa?? Yak Login agtiya, Neen login adre nam kade inda ning saavra newsletter Mannu-Masi Bartave Nin Kharma Agu Agu LOGIN Aaaaguu")
+else:
+    print("Muchkond Browser Close maad makko DANDPINDA")
